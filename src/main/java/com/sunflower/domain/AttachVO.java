99535be19b881789884f4f5fa53f7a704f58083b
hyeon2j <wjsthgus0428@gmail.com>
2024-03-22 00:23:23 +0900

    Feat: detail 페이지 구현 및 tender 처리

---
+++ src/main/java/com/sunflower/domain/AttachVO.java
@@ -0,0 +1,14 @@
+package com.sunflower.domain;
+
+import lombok.Data;
+
+@Data
+public class AttachVO {
+	
+	private String uuid;
+	private int productNum;
+	private String uploadPath;
+	private String uploadFile;
+	private boolean filetype;
+	
+}
